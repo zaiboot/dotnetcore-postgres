ab01c069dddb7760bdaa0853346eab1754f93401
zaiboot <zaiboot@gmail.com>
2019-09-04 18:31:22 -0600

    Cleaning up layout

--- frontend/random-prizes-app/src/app/prize-list/prize-list.component.ts
+++ frontend/random-prizes-app/src/app/prize-list/prize-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PrizeInfo } from '../prize-info';
-import { Status } from '../prize-info'
-import { PrizeInfoComponent } from '../prize-info/prize-info.component'
+import { PrizesService } from '../services/prizes.service';
 
 @Component({
   selector: 'app-prize-list',
@@ -11,21 +10,10 @@ import { PrizeInfoComponent } from '../prize-info/prize-info.component'
 export class PrizeListComponent implements OnInit {
 
   protected prizeList: PrizeInfo[]
-  constructor() { }
+  constructor(private prizesService:PrizesService) { }
 
   ngOnInit() {
-    this.prizeList = [
-      new PrizeInfo(Status.Available, "Zeus1", 10),
-      new PrizeInfo(Status.Available, "Zeus2", 20),
-      new PrizeInfo(Status.Unavailable, "Zeus3", 30),
-      new PrizeInfo(Status.Unavailable, "Zeus4", 30),
-      new PrizeInfo(Status.Unavailable, "Zeus5", 30),
-      new PrizeInfo(Status.Unavailable, "Zeus6", 30),
-      new PrizeInfo(Status.Unavailable, "Zeus7", 30),
-      new PrizeInfo(Status.Unavailable, "Zeus8", 30),
-      new PrizeInfo(Status.Unavailable, "Zeus9", 30),
-    ]
-
+    this.prizeList = this.prizesService.getPrizes()
   }
 
 }
--- frontend/random-prizes-app/src/app/services/prizes.service.ts
+++ frontend/random-prizes-app/src/app/services/prizes.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { PrizeInfo } from '../prize-info';
+import { PrizeInfo, Status } from '../prize-info';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +9,15 @@ export class PrizesService {
   constructor() { }
   
   getPrizes(): PrizeInfo[]{
-
+    return [  new PrizeInfo(Status.Available, "Zeus1", 10),
+    new PrizeInfo(Status.Available, "Zeus2", 20),
+    new PrizeInfo(Status.Unavailable, "Zeus3", 30),
+    new PrizeInfo(Status.Unavailable, "Zeus4", 30),
+    new PrizeInfo(Status.Unavailable, "Zeus5", 30),
+    new PrizeInfo(Status.Unavailable, "Zeus6", 30),
+    new PrizeInfo(Status.Unavailable, "Zeus7", 30),
+    new PrizeInfo(Status.Unavailable, "Zeus8", 30),
+    new PrizeInfo(Status.Unavailable, "Zeus9", 30)
+  ]
   }
 }
