defdb3a55f75f668f9732092d15346657539ca64
zaiboot <zaiboot@gmail.com>
2019-09-08 19:08:10 -0600

    Fixing prize controller tests

--- backend/prizes.unittests/PrizeControllerTests.cs
+++ backend/prizes.unittests/PrizeControllerTests.cs
@@ -7,6 +7,8 @@ using Prizes.Repository;
 using System.Linq;
 using System.Collections.Generic;
 using Prizes.DTO;
+using Moq;
+using System.Threading.Tasks;
 
 namespace Prizes.UnitTests
 {
@@ -20,8 +22,9 @@ namespace Prizes.UnitTests
             Action<AutoMock, PrizeController> action = async (AutoMock m, PrizeController system) =>
             {
                 const int PRIZES_COUNT = 10;
-                AndWhenISetUpPrizes(PRIZES_COUNT, m);
+                AndWhenISetUpPrizes(PRIZES_COUNT, m, 1);
                 AndISetupAMapping<IEnumerable<Prize>, List<Models.Prize>>(m, GivenAListOfModelPrize(PRIZES_COUNT));
+                AndWhenIMarkOneAsAvailable(m);
                 var r = await system.GetAsync(1);
                 Assert.NotNull(r);
                 Assert.NotNull(r.Value);
@@ -29,9 +32,16 @@ namespace Prizes.UnitTests
                 Assert.True(prizes.Any());
             };
             Execute(action);
-        }       
+        }
+        private void AndWhenIMarkOneAsAvailable(AutoMock m)
+        {
+            m.Mock<IPrizeRepository>()
+                .Setup(s => s.MarkOneAsAvailable(It.IsAny<Prize>()))
+                    .Returns(Task.CompletedTask);
 
-        private void AndWhenISetUpPrizes(int count, AutoMock m)
+
+        }
+        private void AndWhenISetUpPrizes(int count, AutoMock m, int customerId)
         {
             var prizes = new List<Prize>();
             for (int i = 0; i < count; i++)
@@ -39,7 +49,7 @@ namespace Prizes.UnitTests
                 var p = GivenASinglePrize(i);
                 prizes.Add(p);
             }
-            m.Mock<IPrizeRepository>().Setup(s => s.GetPrizes()).Returns(prizes);
+            m.Mock<IPrizeRepository>().Setup(s => s.GetPrizes(customerId)).Returns(prizes);
 
         }
         private List<Models.Prize> GivenAListOfModelPrize(int count)
@@ -75,8 +85,12 @@ namespace Prizes.UnitTests
         [Fact]
         public void CustomerIsValid()
         {
-            Action<AutoMock, PrizeController> action = async ( m, system) =>
-            {                
+            Action<AutoMock, PrizeController> action = async (m, system) =>
+            {
+                const int PRIZES_COUNT = 10;
+                AndWhenISetUpPrizes(PRIZES_COUNT, m, 1);
+                AndISetupAMapping<IEnumerable<Prize>, List<Models.Prize>>(m, GivenAListOfModelPrize(PRIZES_COUNT));
+                AndWhenIMarkOneAsAvailable(m);
                 var r = await system.GetAsync(1);
                 Assert.NotNull(r);
                 Assert.NotNull(r.Value);
